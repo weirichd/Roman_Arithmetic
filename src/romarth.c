3cdf6cf0bad4e11ad1116f63e84f76234b79b2ff
David E. Weirich <weirich.david@gmail.com>
2016-07-07 16:21:03 -0400

    I + II = II test passes

--- src/romarth.c
+++ src/romarth.c
@@ -1,15 +1,25 @@
 #include "romarth.h"
 #include <stddef.h> /* For NULL Macro */
+#include <string.h> /* For strlen, memset */
 
+static int roman_to_arabic(char *numeral);
+static char *arabic_to_roman(char *dest, int number);
 
 char *roman_add(char *sum, char *summand1, char *summand2) {
-    sum[0] = 'I';
-    sum[1] = 'I';
-
-    return sum;
+    int a = roman_to_arabic(summand1);
+    int b = roman_to_arabic(summand2);
+    return arabic_to_roman(sum, a + b);
 }
 
 char *roman_subtract(char *differene, char *minuend, char *suptrahend) {
     return NULL;
 }
 
+static int roman_to_arabic(char *numeral) {
+    return strlen(numeral); 
+}
+
+static char *arabic_to_roman(char *dest, int number) {
+    memset(dest,'I', number);
+    return dest;
+}
