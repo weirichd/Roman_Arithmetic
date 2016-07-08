5ce53d489431f293c19205303d7dd9251b9a6aa8
David E. Weirich <weirich.david@gmail.com>
2016-07-07 22:01:54 -0400

    Added Failing test for L

--- test/check_romarth.c
+++ test/check_romarth.c
@@ -102,6 +102,17 @@ START_TEST(I_plus_VIII_equals_IX) {
     ck_assert_str_eq(sum, "IX");
 } END_TEST
 
+START_TEST(XXV_plus_XXV_equals_L) { 
+    char summand1[] = "XXV";
+    char summand2[] = "XXV";
+    char sum[2] = {};
+
+    roman_add(sum, summand1, summand2); 
+
+    ck_assert_str_eq(sum, "L");
+} END_TEST
+
+
 int main() {
     Suite *s = suite_create("Arithmatic");
     TCase *tc_add = tcase_create("Addition");
@@ -121,7 +132,9 @@ int main() {
     tcase_add_test(tc_add, IV_plus_I_equals_V);
     tcase_add_test(tc_add, IX_plus_I_equals_X);
     tcase_add_test(tc_add, II_plus_II_equals_IV);
+
     tcase_add_test(tc_add, I_plus_VIII_equals_IX);
+    tcase_add_test(tc_add, XXV_plus_XXV_equals_L);
 
     srunner_run_all(sr, CK_NORMAL);
     num_fails = srunner_ntests_failed(sr);
