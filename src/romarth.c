1ab2dd5f91e8e35f9c5c75c080267b57677a9600
David Edward Weirich <weirich.david@gmail.com>
2016-07-12 16:49:36 -0400

    Addition now allows for the an input to be an output too

--- src/romarth.c
+++ src/romarth.c
@@ -35,10 +35,12 @@ char *roman_add(char *const sum, const size_t sum_size, const char *const summan
     int a = roman_to_arabic(summand1);
     int b = roman_to_arabic(summand2);
 
+    memset(sum, 0, sum_size);
+
     if(a + b <= LARGEST_NUMERAL_THAT_CAN_BE_EXPRESSED) 
         return arabic_to_roman(sum, sum_size, a + b);
     else
-        return memset(sum, 0, sum_size);
+        return sum;
 }
 
 char *roman_subtract(char *const difference, const size_t difference_size, const char *const minuend, const char *const suptrahend) {
