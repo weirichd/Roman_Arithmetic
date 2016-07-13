ec1c86696dea77f7d296e382e774a411acf2ec20
David Edward Weirich <weirich.david@gmail.com>
2016-07-13 12:33:18 -0400

    CM test passes, this is is the final subtractive rule test

--- src/romarth.c
+++ src/romarth.c
@@ -9,30 +9,9 @@ typedef struct RomanMapEntry {
     int arabic_value;
 } RomanMapEntry;
 
-/*****************
-  Commented out for the purpose of test driving subtraction
- *****************
-
 static const RomanMapEntry ROMAN_MAP[] = {
     { .roman_symbol = "M",  .arabic_value = 1000},
     { .roman_symbol = "CM", .arabic_value = 900},
-    { .roman_symbol = "D",  .arabic_value = 500},
-    { .roman_symbol = "CD", .arabic_value = 400},
-    { .roman_symbol = "C",  .arabic_value = 100},
-    { .roman_symbol = "XC", .arabic_value = 90},
-    { .roman_symbol = "L",  .arabic_value = 50},
-    { .roman_symbol = "XL", .arabic_value = 40},
-    { .roman_symbol = "X",  .arabic_value = 10},
-    { .roman_symbol = "IX", .arabic_value = 9},
-    { .roman_symbol = "V",  .arabic_value = 5},
-    { .roman_symbol = "IV", .arabic_value = 4},
-    { .roman_symbol = "I",  .arabic_value = 1},
-};
-
-*/
-
-static const RomanMapEntry ROMAN_MAP[] = {
-    { .roman_symbol = "M",  .arabic_value = 1000},
     { .roman_symbol = "D",  .arabic_value = 500},
     { .roman_symbol = "CD", .arabic_value = 400},
     { .roman_symbol = "C",  .arabic_value = 100},
