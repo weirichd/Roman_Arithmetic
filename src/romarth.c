f4673a5f463f63de5907f822d0a61012d981d2dd
David Edward Weirich <weirich.david@gmail.com>
2016-07-14 11:46:06 -0400

    Refactored NULL check code

--- src/romarth.c
+++ src/romarth.c
@@ -32,35 +32,31 @@ static const size_t ROMAN_MAP_SIZE = sizeof(ROMAN_MAP)/sizeof(RomanMapEntry);
 static const int LARGEST_NUMERAL_THAT_CAN_BE_EXPRESSED = 3999;
 
 char *roman_add(char *const sum, const size_t sum_size, const char *const summand1, const char *const summand2) {
-    if(sum != NULL) {
-        if(summand1 == NULL || summand2 == NULL)
-            return NULL;
+    if(sum == NULL || summand1 == NULL || summand2 == NULL)
+        return NULL;
 
-        int a = roman_to_arabic(summand1);
-        int b = roman_to_arabic(summand2);
+    int a = roman_to_arabic(summand1);
+    int b = roman_to_arabic(summand2);
 
-        memset(sum, 0, sum_size);
+    memset(sum, 0, sum_size);
 
-        if(a + b <= LARGEST_NUMERAL_THAT_CAN_BE_EXPRESSED) 
-            arabic_to_roman(sum, sum_size, a + b);
-    }
+    if(a + b <= LARGEST_NUMERAL_THAT_CAN_BE_EXPRESSED) 
+        arabic_to_roman(sum, sum_size, a + b);
 
     return sum;
 }
 
 char *roman_subtract(char *const difference, const size_t difference_size, const char *const minuend, const char *const suptrahend) {
-    if(difference != NULL) {
-        if(minuend == NULL || suptrahend == NULL)
-            return NULL;
+    if(difference == NULL || minuend == NULL || suptrahend == NULL)
+        return NULL;
 
-        int a = roman_to_arabic(minuend);
-        int b = roman_to_arabic(suptrahend);
+    int a = roman_to_arabic(minuend);
+    int b = roman_to_arabic(suptrahend);
 
-        memset(difference, 0, difference_size);
+    memset(difference, 0, difference_size);
 
-        if(a > b)
-            arabic_to_roman(difference, difference_size, a - b);
-    }
+    if(a > b)
+        arabic_to_roman(difference, difference_size, a - b);
 
     return difference;
 }
