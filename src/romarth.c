d702d2702ba7a8f3d2ea3bbfd5a2d607854b9a5b
David Edward Weirich <weirich.david@gmail.com>
2016-07-17 18:44:19 -0400

    Made code DRYer by refactoring addition and subtraction into a single function

--- src/romarth.c
+++ src/romarth.c
@@ -4,39 +4,48 @@
 
 #include <string.h> /* memset */
 
-static const int LARGEST_NUMERAL_THAT_CAN_BE_EXPRESSED = 3999;
+typedef int (*Operation)(int a, int b);
+
+static char *roman_Operation(char *const result, const size_t result_size, const char *const operand1, const char *const operand2, Operation operation);
+
+static int add(int a, int b) {
+    return a + b;
+}
+
+static int subtract(int a, int b) {
+    return a - b;
+}
 
 char *roman_add(char *const sum, const size_t sum_size, const char *const summand1, const char *const summand2) {
-    if(sum == NULL || summand1 == NULL || summand2 == NULL)
-        return NULL;
-
-    if(is_a_valid_roman_numeral(summand1) && is_a_valid_roman_numeral(summand2)) {
-        int a = roman_to_arabic(summand1);
-        int b = roman_to_arabic(summand2);
-
-        memset(sum, 0, sum_size);
-
-        if(a + b <= LARGEST_NUMERAL_THAT_CAN_BE_EXPRESSED) 
-            arabic_to_roman(sum, sum_size, a + b);
-     } else
-        memset(sum, 0, sum_size);
-
-    return sum;
+    return roman_Operation(sum, sum_size, summand1, summand2, add);
 }
 
 char *roman_subtract(char *const difference, const size_t difference_size, const char *const minuend, const char *const suptrahend) {
-    if(difference == NULL || minuend == NULL || suptrahend == NULL)
+    return roman_Operation(difference, difference_size, minuend, suptrahend, subtract);
+}
+
+static const int LARGEST_NUMERAL_THAT_CAN_BE_EXPRESSED = 3999;
+
+inline int is_within_valid_range(int number) {
+    return 0 < number && number < LARGEST_NUMERAL_THAT_CAN_BE_EXPRESSED;
+}
+
+static char *roman_Operation(char *const result, const size_t result_size, const char *const operand1, const char *const operand2, Operation operation) {
+    if(result == NULL || operand1 == NULL || operand2 == NULL)
         return NULL;
 
-    if(is_a_valid_roman_numeral(minuend) && is_a_valid_roman_numeral(suptrahend)) {
-        int a = roman_to_arabic(minuend);
-        int b = roman_to_arabic(suptrahend);
+    if(is_a_valid_roman_numeral(operand1) && is_a_valid_roman_numeral(operand2)) {
+        int a = roman_to_arabic(operand1);
+        int b = roman_to_arabic(operand2);
 
-        memset(difference, 0, difference_size);
+        memset(result, 0, result_size);
 
-        if(a > b)
-            arabic_to_roman(difference, difference_size, a - b);
+        int arabic_result = operation(a, b);
+
+        if(is_within_valid_range(arabic_result)) 
+            arabic_to_roman(result, result_size, arabic_result);
      } else
-        memset(difference, 0, difference_size);
-    return difference;
+        memset(result, 0, result_size);
+
+    return result;
 }
