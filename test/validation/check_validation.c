150d6e4ca97fe7e0e703f93f51e7a13ecbbcb27d
David Edward Weirich <weirich.david@gmail.com>
2016-07-17 17:54:10 -0400

    Added a few more redundandt tests

--- test/validation/check_validation.c
+++ test/validation/check_validation.c
@@ -22,6 +22,11 @@ int main() {
     tcase_add_test(tc_validation, MMXLVI_is_considered_valid);
     tcase_add_test(tc_validation, IXI_is_considered_invalid);
     tcase_add_test(tc_validation, IVI_is_considered_invalid);
+    tcase_add_test(tc_validation, IM_is_considered_invalid);
+    tcase_add_test(tc_validation, VV_is_considered_invalid);
+
+    tcase_add_test(tc_validation, DD_is_considered_invalid);
+    tcase_add_test(tc_validation, IXCD_is_considered_invalid);
 
     srunner_run_all(sr, CK_NORMAL);
     num_fails = srunner_ntests_failed(sr);
--- test/validation/validation_tests.c
+++ test/validation/validation_tests.c
@@ -46,3 +46,26 @@ START_TEST(IVI_is_considered_invalid) {
     ck_assert(!validation_result); 
 } END_TEST
 
+START_TEST(IM_is_considered_invalid) {
+   int validation_result = is_a_valid_roman_numeral("IM");
+
+    ck_assert(!validation_result); 
+} END_TEST
+
+START_TEST(VV_is_considered_invalid) {
+   int validation_result = is_a_valid_roman_numeral("VV");
+
+    ck_assert(!validation_result); 
+} END_TEST
+
+START_TEST(DD_is_considered_invalid) {
+   int validation_result = is_a_valid_roman_numeral("DD");
+
+    ck_assert(!validation_result); 
+} END_TEST
+
+START_TEST(IXCD_is_considered_invalid) {
+   int validation_result = is_a_valid_roman_numeral("IXCD");
+
+    ck_assert(!validation_result); 
+} END_TEST
