1fc80ee789aab7a1f29d78a12c584bd844dc8432
David Edward Weirich <weirich.david@gmail.com>
2016-07-17 15:38:37 -0400

    Can now invalidate strings that have non-roman characters

--- src/validation.h
+++ src/validation.h
@@ -4,9 +4,18 @@
 #include "numeral_map.h"
 
 static int is_a_valid_roman_numeral(const char *const numeral) {
-    const RomanMapEntry *entry = find_map_element(numeral);
+    int numeral_offset = 0;
 
-    return entry != NULL;
+    while(numeral[numeral_offset] != '\0') {
+        const RomanMapEntry *current_entry = find_map_element(numeral + numeral_offset);
+
+        if(current_entry == NULL)
+            return 0;
+
+        numeral_offset += strlen(current_entry->roman_symbol);
+    }
+
+    return 1;
 }
 
 #endif /* VALIDATION_H */
