545d974e7483db7bb58b425294bc5a8d1a33bba2
David Edward Weirich <weirich.david@gmail.com>
2016-07-17 13:00:14 -0400

    ACTUALLY fixed the test, it had another dumb typo

--- test/arabic_to_roman_conversion_tests.c
+++ test/arabic_to_roman_conversion_tests.c
@@ -87,7 +87,7 @@ START_TEST(arabic_to_roman_returns_an_empty_string_when_there_isnt_enough_room_i
 } END_TEST
 
 START_TEST(the_longest_possible_roman_numeral_will_convert_properly) {
-    char expected[] = "MMMDCCLXXVII";
+    char expected[] = "MMMDCCCLXXXVIII";
     char actual[16] = { };
 
     arabic_to_roman(actual, ARRAY_LENGTH(actual), 3888);
